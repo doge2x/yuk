a2603f1b9c43f7a81f34d46c46d040d5c90a3552
doge2x <doge2x@163.com>
2021-11-16 08:17:47 +0800

    test: improve the tests

--- tests/test-server.js
+++ tests/test-server.js
@@ -1,25 +1,26 @@
 const YUK_ADDR = "ws://127.0.0.1:9009";
-const WS_NUM = 100;
+const WS_NUM = 150;
+
+const connections = new Map();
 
 function new_ws(id) {
   const ws = new WebSocket(YUK_ADDR);
   ws.onmessage = (e) => console.log(e.data);
-  ws.onopen = () => {
-    ws.send(`#${id}: hello`);
-    console.info(`#${id}: connected`);
-  };
-  ws.onclose = () => console.info(`#${id}: closed`);
-  return ws;
+  ws.onclose = () => console.info(`#${id} closed, ${connections.size} left`);
+  return new Promise((resolve, _) => (ws.onopen = (_) => resolve(ws)));
 }
 
-const connections = new Map();
-
 setInterval(() => {
-  let id = Math.floor(Math.random() * WS_NUM);
-  if (connections.has(id)) {
+  const id = Math.floor(Math.random() * WS_NUM);
+  const conn = connections.get(id);
+  if (conn === undefined) {
+    new_ws(id).then((ws) => {
+      ws.send(`#${id}: hello`);
+      console.info(`#${id}: connected`);
+      connections.set(id, ws);
+    });
+  } else {
     connections.get(id).close();
     connections.delete(id);
-  } else {
-    connections.set(id, new_ws(id));
   }
-}, 500);
+}, 100);
