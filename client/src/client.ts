c25dbe56c808ca26149cd4890a90678fd1e8612a
doge2x <doge2x@163.com>
2022-05-07 16:28:50 +0800

    feat(server): only send updated answers

--- client/src/client.ts
+++ client/src/client.ts
@@ -2,7 +2,7 @@ import { Answer, UserAnswer } from "./types";
 import { JSONRPCClient } from "json-rpc-2.0";
 import GM from "./gm";
 import { devLog } from "./utils";
-import { SERVER, USERNAME } from "./config";
+import { SERVER, USERNAME, TOKEN } from "./config";
 
 export class Client {
   // Use http request, since we can use non-secure connections
@@ -12,12 +12,7 @@ export class Client {
   private onmsg: ((msg: UserAnswer[]) => void)[] = [];
   private queue: Map<number, Answer> = new Map();
 
-  private constructor(client: JSONRPCClient, examId: number) {
-    this.client = client;
-    this.examId = examId;
-  }
-
-  static async login(examId: number): Promise<Client> {
+  constructor(examId: number) {
     const client = new JSONRPCClient(async (req) => {
       const url = SERVER.value;
       if (url !== undefined) {
@@ -42,7 +37,8 @@ export class Client {
         });
       }
     });
-    return new Client(client, examId);
+    this.client = client;
+    this.examId = examId;
   }
 
   async watch(ms: number) {
@@ -57,9 +53,10 @@ export class Client {
   }
 
   private async sendQueue() {
+    const username = USERNAME.value;
     if (
-      this.queue.size <= 0 ||
-      USERNAME.value === undefined ||
+      this.queue.size < 1 ||
+      username === undefined ||
       SERVER.value === undefined
     ) {
       return;
@@ -67,19 +64,32 @@ export class Client {
     let answers: UserAnswer[] = [];
     for (const { problem_id, result } of this.queue.values()) {
       answers.push({
-        username: USERNAME.value,
+        username: username,
         problem_id: problem_id,
         result: result,
       });
     }
     this.queue.clear();
+    if (TOKEN.value === undefined) {
+      devLog(`login to server: ${username}, ${this.examId}`);
+      const token = await this.client.request("login", [
+        USERNAME.value,
+        this.examId,
+      ]);
+      devLog("got token", token);
+      TOKEN.value = token;
+    }
     devLog("send answers", answers);
     const rcev = await this.client.request("answer_problem", [
-      this.examId,
+      TOKEN.value,
       answers,
     ]);
-    devLog("receive answers", rcev);
-    this.onmsg.forEach((cb) => cb(rcev));
+    this.pushMsg(rcev);
+  }
+
+  private pushMsg(answers: UserAnswer[]) {
+    devLog("receive answers", answers);
+    this.onmsg.forEach((cb) => cb(answers));
   }
 
   async answerProblem(answers: Answer[]) {
