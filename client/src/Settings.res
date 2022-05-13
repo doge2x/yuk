7d8537df5b648054cd720fed27645ee7c1b9a4b6
doge2x <doge2x@163.com>
2022-05-13 15:34:42 +0800

    feat(client): use ReScript to rewrite `settings.tsx`

---
+++ client/src/Settings.res
@@ -0,0 +1,121 @@
+@module
+external style: {..} = "./style.mod.css"
+
+module React = Recks
+module ReactDOMRe = Recks.DOMRe
+
+type entryType =
+  | Text(option<string>)
+  | Checkbox(option<bool>)
+
+module Entry = {
+  @react.component
+  let make = (
+    ~name: string,
+    ~title: string,
+    ~ty: entryType,
+    ~required: option<bool>=?,
+    ~pattern: option<string>=?,
+  ) =>
+    <div className={style["settingsEntry"]}>
+      <label htmlFor={name}> {React.string(title)} </label>
+      {
+        let (ty, value, checked) = switch ty {
+        | Text(value) => ("text", value, None)
+        | Checkbox(checked) => ("checkbox", None, checked)
+        }
+        <input name title type_=ty ?value ?checked ?required ?pattern />
+      }
+    </div>
+}
+
+module TitleText = {
+  @react.component
+  let make = (~title: string, ~content: option<string>=?) =>
+    <p>
+      <strong> {React.string(title)} </strong>
+      {content->Belt.Option.map(React.string)->Belt.Option.getWithDefault(React.null)}
+    </p>
+}
+
+module UList = {
+  @react.component
+  let make = (~children: React.element) =>
+    <ul>
+      {React.Children.toArray(children)
+      ->Js.Array2.map(item => {
+        <li> item </li>
+      })
+      ->React.array}
+    </ul>
+}
+
+// TODO: use pure rescript?
+%%raw(`
+  import {
+    NO_LEAVE_CHECK,
+    SERVER,
+    SORT_PROBLEMS,
+    SYNC_ANSWERS,
+    USERNAME,
+  } from "./config";
+`)
+
+let make = () => {
+  <div>
+    <form
+      onSubmit={ev =>
+        // TODO: use pure rescript?
+        %raw(`
+          (ev) => {
+            ev.preventDefault();
+            const form = new FormData(this);
+            USERNAME.value = form.get("username");
+            SERVER.value = form.get("server");
+            NO_LEAVE_CHECK.value = form.get("no_leave_check") === "on";
+            SORT_PROBLEMS.value = form.get("sort_problems") === "on";
+            SYNC_ANSWERS.value = form.get("sync_answers") === "on";
+          }
+        `)(ev)}>
+      <Entry name="username" title=`用户名` ty=Text(Some(%raw(`USERNAME.value`))) />
+      <Entry name="server" title=`服务器` ty=Text(Some(%raw(`SERVER.value`))) />
+      <Entry
+        name="sync_answers" title=`同步答案` ty=Checkbox(Some(%raw(`SYNC_ANSWERS.value`)))
+      />
+      <Entry
+        name="sort_problems" title=`排序答案` ty=Checkbox(Some(%raw(`SORT_PROBLEMS.value`)))
+      />
+      <Entry
+        name="no_leave_check"
+        title=`拦截切屏检测`
+        ty=Checkbox(Some(%raw(`NO_LEAVE_CHECK.value`)))
+      />
+      <div className={style["submitTip"]}>
+        <i> {React.string(`*更改设置后请刷新页面`)} </i>
+      </div>
+      <div className={style["settingsSubmit"]}> <button> {React.string(`提交`)} </button> </div>
+    </form>
+    <div className={style["about"]}>
+      <TitleText title=`功能特性：` />
+      <UList>
+        <TitleText
+          title=`同步答案：`
+          content=`点击题目显示详细答案，在选项/填空处悬停显示简略答案`
+        />
+        <TitleText
+          title=`排序题目：` content=`根据 ID 对题目和选项进行重新排序`
+        />
+        <TitleText
+          title=`拦截切屏检测：` content=`随意切换页面、窗口不会被发现`
+        />
+        <TitleText
+          title=`拦截上传截图：` content=`仅当用户确认后，才会上传截图`
+        />
+        <TitleText
+          title=`拦截异常状态：`
+          content=`即使本地显示异常也不会推送到服务器`
+        />
+      </UList>
+    </div>
+  </div>
+}
--- client/src/recks.ts
+++ client/src/recks.ts
@@ -1,54 +1,47 @@
-type HTMLEventProp<K extends keyof HTMLElementTagNameMap> = {
-  [E in keyof HTMLElementEventMap as `on-${E}`]?: (
-    this: HTMLElementTagNameMap[K],
-    ev: HTMLElementEventMap[E]
-  ) => void;
-};
-type ExcludeHTMLProp = "children" | "style" | "classList";
-type ExtendHTMLProp = {
-  classList?: string[];
-  style?: StyleProp;
-  dangerouslySetInnerHTML?: { __html: string };
-};
-type HTMLProp<K extends keyof HTMLElementTagNameMap> = {
-  [A in keyof HTMLElementTagNameMap[K] as Exclude<
-    A,
-    ExcludeHTMLProp
-  >]?: HTMLElementTagNameMap[K][A];
-} & HTMLEventProp<K> &
-  ExtendHTMLProp;
-type StyleProp = {
-  [S in keyof CSSStyleDeclaration]?: CSSStyleDeclaration[S];
-};
+declare global {
+  namespace Recks {
+    export type PropWithChildren<T> = T & { children?: any };
 
-declare namespace Recks {
-  type Child = Recks.Element | string | number;
-  type Children = Child | Child[];
-  type Element = HTMLElement | DocumentFragment;
-  type PropWithChildren<T> = T & { children?: Children };
+    namespace JSX {
+      type Element = Node;
 
-  namespace JSX {
-    type Element = Recks.Element;
-    interface ElementChildrenAttribute {
-      children: any;
+      interface ElementChildrenAttribute {
+        children?: any;
+      }
+
+      type IntrinsicElements = {
+        [K in keyof globalThis.JSX.IntrinsicElements]: PropWithChildren<{
+          [P in keyof globalThis.JSX.IntrinsicElements[K] as Exclude<
+            P,
+            "children"
+          >]: globalThis.JSX.IntrinsicElements[K][P];
+        }>;
+      };
     }
-    type IntrinsicElements = {
-      [K in keyof HTMLElementTagNameMap]: PropWithChildren<HTMLProp<K>>;
-    };
   }
 }
 
-function addChildren(parent: Node, children: Recks.Child[]): void {
+function childrenToArray(children: any): Recks.JSX.Element[] {
+  if (Array.isArray(children)) {
+    return new Array<Node>().concat(...children.map(childrenToArray));
+  } else if (children instanceof Node) {
+    return [children];
+  } else if (children === null || children === undefined) {
+    return [];
+  } else {
+    return [document.createTextNode(String(children))];
+  }
+}
+
+type Props = { [k: string]: any };
+
+function addChildren(parent: Node, children: Recks.JSX.Element[]) {
   for (const child of children) {
-    if (child instanceof Node) {
-      parent.appendChild(child);
-    } else {
-      parent.appendChild(document.createTextNode(String(child)));
-    }
+    parent.appendChild(child);
   }
 }
 
-function setCSSProps(ele: HTMLElement, style: StyleProp): void {
+function setCSSProps(ele: HTMLElement, style: CSSStyleDeclaration) {
   for (const [name, value] of Object.entries(style)) {
     if (name.startsWith("-")) {
       ele.style.setProperty(name, value as any);
@@ -58,65 +51,61 @@ function setCSSProps(ele: HTMLElement, style: StyleProp): void {
   }
 }
 
-function flattenChildren(children: Recks.Children[]): Recks.Child[] {
-  let flatten: Recks.Child[] = [];
-  for (const ele of children) {
-    if (Array.isArray(ele)) {
-      flatten = flatten.concat(ele);
-    } else {
-      flatten.push(ele);
+function setDOMProps(ele: HTMLElement, props: Props) {
+  for (const [name, value] of Object.entries(props)) {
+    if (value === undefined) {
+      continue;
+    }
+    switch (name) {
+      case "class":
+      case "className":
+        ele.setAttribute("class", value);
+        break;
+      case "style":
+        setCSSProps(ele, value);
+        break;
+      case "dangerouslySetInnerHTML":
+        ele.innerHTML = value.__html;
+        break;
+      default:
+        if (name.startsWith("on")) {
+          ele.addEventListener(name.slice(2).toLowerCase(), value);
+        } else {
+          ele.setAttribute(name, value);
+        }
     }
   }
-  return flatten;
 }
 
-type Props = { [k: string]: any };
+export function createElement(
+  t: string | ((props: Props) => Recks.JSX.Element),
+  props: Props | null | undefined,
+  ...children: any[]
+): Recks.JSX.Element {
+  let nodeArray = childrenToArray(children);
+  if (typeof t === "function") {
+    return t({ ...props, children: nodeArray });
+  } else {
+    const ele = document.createElement(t);
+    addChildren(ele, nodeArray);
+    setDOMProps(ele, props ?? {});
+    return ele;
+  }
+}
+
+export function Fragment(props: Recks.PropWithChildren<{}>): DocumentFragment {
+  let frag = document.createDocumentFragment();
+  addChildren(frag, childrenToArray(props.children));
+  return frag;
+}
+
+export const Children = {
+  toArray: childrenToArray,
+};
 
 const Recks = {
-  createElement<K extends keyof HTMLElementTagNameMap>(
-    t: K | ((props: Props) => Recks.Element),
-    props: Props | null,
-    ...children: Recks.Children[]
-  ): Recks.Element {
-    let flatten = flattenChildren(children);
-    if (typeof t === "function") {
-      return t({ ...props, children: flatten });
-    } else {
-      const ele = document.createElement(t);
-      addChildren(ele, flatten);
-      for (const [name, value] of Object.entries(props ?? {})) {
-        if (value === undefined) {
-          continue;
-        }
-        switch (name) {
-          case "classList":
-            ele.classList.add(...value);
-            break;
-          case "style":
-            setCSSProps(ele, value);
-            break;
-          case "dangerouslySetInnerHTML":
-            ele.innerHTML = value.__html;
-            break;
-          default:
-            if (name.startsWith("on-")) {
-              ele.addEventListener(name.slice(3), value);
-            } else if (name in ele) {
-              (ele as any)[name] = value;
-            }
-        }
-      }
-      return ele;
-    }
-  },
-
-  Fragment(props: { children: any }): DocumentFragment {
-    let frag = document.createDocumentFragment();
-    if (Array.isArray(props.children)) {
-      addChildren(frag, props.children);
-    }
-    return frag;
-  },
+  createElement: createElement,
+  Fragment: Fragment,
 };
 
 export default Recks;
