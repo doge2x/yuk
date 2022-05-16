70048e16b5f0b8d218f673468cbf43d7985868cc
doge2x <doge2x@163.com>
2022-05-16 21:51:28 +0800

    chore(client): add a icon

--- client/webpack.config.ts
+++ client/webpack.config.ts
@@ -8,6 +8,7 @@ const banner = `\
 // @name         ${pkg.name}
 // @version      ${pkg.version}
 // @author       doge2x
+// @icon         https://www.yuketang.cn/static/images/favicon.ico
 // @namespace    https://github.com/doge2x/yuk
 // @match        https://examination.xuetangx.com/*
 // @grant        GM.xmlHttpRequest
