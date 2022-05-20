5c165ce7f9604efdd1451314a7d3e03e094c5b91
Doge2x <doge2x@outlook.com>
2022-05-20 10:31:56 +0800

    chore(release): 0.5.0

--- dist/yuk.user.js
+++ dist/yuk.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         yuk-client
-// @version      0.4.0
+// @version      0.5.0
 // @author       doge2x
 // @icon         https://www.yuketang.cn/static/images/favicon.ico
 // @match        https://examination.xuetangx.com/*
